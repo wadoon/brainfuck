e6ebbc09293e1771d4b2b5c5c84a5bda74ede828
Alexander Weigl <spamling@web.de>
2010-03-17 04:51:24 +0100

    change bcel to built-in packages
    additional added three shell scripts

--- src/weigl/bf/BFCLI.java
+++ src/weigl/bf/BFCLI.java
@@ -5,40 +5,48 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 
+/**
+ * 
+ * @author Alexander Weigl
+ * @date a-long-time-ago
+ */
 public class BFCLI {
-	public BFCLI() throws IOException {
+    public BFCLI(String file) throws IOException {
+	BufferedReader br = new BufferedReader(new FileReader(new File(file)));
+	StringBuffer b = new StringBuffer();
+	String tmp = null;
 
-		BufferedReader br = new BufferedReader(new FileReader(
-				new File("Code.b")));
-		StringBuffer b = new StringBuffer();
-		String tmp = null;
+	while ((tmp = br.readLine()) != null)
+	    b.append(tmp);
 
-		while ((tmp = br.readLine()) != null)
-			b.append(tmp);
-		
-		tmp = b.toString();
-		BFInterpreter interpreter = new BFInterpreter(tmp);
-		interpreter.setInOut(new CommandLineIO());
-		interpreter.run();
-	}
+	tmp = b.toString();
+	
+	BFInterpreter interpreter = new BFInterpreter(tmp);
+	interpreter.setInOut(new CommandLineIO());
+	interpreter.run();
+    }
 
-	public static void main(String[] args) throws Exception {
-		new BFCLI();
-	}
+    public static void main(String[] args) throws Exception {
+	if (args.length == 1)
+	    new BFCLI(args[0]);
+	else
+	    System.err.println("You have to specify an brainfuck script.\n"
+		    + "Usage: bfrun <script>\n");
+    }
 }
 
 class CommandLineIO implements IOInterface {
 
-	public char read() {
-		try {
-			return (char) System.in.read();
-		} catch (IOException e) {
-			e.printStackTrace();
-			return 0;
-		}
+    public char read() {
+	try {
+	    return (char) System.in.read();
+	} catch (IOException e) {
+	    e.printStackTrace();
+	    return 0;
 	}
+    }
 
-	public void write(char c) {
-		System.out.print(c);
-	}
+    public void write(char c) {
+	System.out.print(c);
+    }
 }
